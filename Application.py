d6f7e92d4b4f67a4d79dfbc724c7dc39f190e7ae
Connor Lin <zhizhenl@andrew.cmu.edu>
2019-04-08 15:05:30 -0400

    added support for multiple pokemon

--- Application.py
+++ Application.py
@@ -25,7 +25,7 @@ class Application(Frame):
         self.pokedexBtn = Button(self, text="See All Pokemon", command=self.seePokedex)
         self.pokedexBtn.grid(row=0, column=1)
 
-        self.checkBtn = Button(self, text="Lock In", command=self.checkPokemon)
+        self.checkBtn = Button(self, text="Lock in", command=self.checkPokemon)
         self.checkBtn.grid(row=1, column=1)
 
         self.battleBtn = Button(self, text="Begin Battle", state=DISABLED, command=self.beginBattle)
@@ -83,8 +83,10 @@ class Application(Frame):
 
         # Pokemon Objects
         # Blank until the user enters which Pokemon they want
-        self.userPokemon = None
-        self.cpuPokemon = None
+        self.userPokemon = []
+        self.userActive = 0;
+        self.cpuPokemon = []
+        self.cpuActive = 0;
 
     # Creating a method to print the list of all Pokemon
     def seePokedex(self):
@@ -98,38 +100,137 @@ class Application(Frame):
     # Returns an error message if the Pokemon are not usable
     def checkPokemon(self):
         if self.userStrVar != "" and self.cpuStrVar != "":
-            if self.userStrVar.get().lower() in pokedex and self.cpuStrVar.get().lower() in pokedex:
-                self.txtStats.config(state=NORMAL)
-                self.txtStats.delete(0.0, END)
-                self.txtStats.insert(0.0, "You are ready to battle.")
-                self.txtStats.config(state=DISABLED)
+            userPokemon = self.userStrVar.get().lower().split(",")
+            userPokemon = [poke.strip() for poke in userPokemon]
 
-                self.checkBtn.config(state=DISABLED)
-                self.entName1.config(state=DISABLED)
-                self.entName2.config(state=DISABLED)
-                self.pokedexBtn.config(state=DISABLED)
-                self.battleBtn.config(state=NORMAL)
+            cpuPokemon = self.cpuStrVar.get().lower().split(",")
+            cpuPokemon = [poke.strip() for poke in cpuPokemon]
 
-                self.userPokemon = Pokemon(self.userStrVar.get())
-                self.cpuPokemon = Pokemon(self.cpuStrVar.get())
+            # check that the pokemon are valid
+            for poke in userPokemon:
+                if poke not in pokedex:
+                    self.txtStats.config(state=NORMAL)
+                    self.txtStats.delete(0.0, END)
+                    self.txtStats.insert(0.0, "Please make sure the Pokemon you've entered are in the Pokedex: " + poke)
+                    self.txtStats.config(state=DISABLED)
+                    self.battleBtn.config(state=DISABLED)
+                    return
 
-            # Returning an error message if Pokemon are not valid
+            for poke in cpuPokemon:
+                if poke not in pokedex:
+                    self.txtStats.config(state=NORMAL)
+                    self.txtStats.delete(0.0, END)
+                    self.txtStats.insert(0.0, "Please make sure the Pokemon you've entered are in the Pokedex: " + poke)
+                    self.txtStats.config(state=DISABLED)
+                    self.battleBtn.config(state=DISABLED)
+                    return
+
+            self.txtStats.config(state=NORMAL)
+            self.txtStats.delete(0.0, END)
+            self.txtStats.insert(0.0, "You are ready to battle.")
+            self.txtStats.config(state=DISABLED)
+
+            self.checkBtn.config(state=DISABLED)
+            self.entName1.config(state=DISABLED)
+            self.entName2.config(state=DISABLED)
+            self.pokedexBtn.config(state=DISABLED)
+            self.battleBtn.config(state=NORMAL)
+
+            for poke in userPokemon:
+                self.userPokemon.append(Pokemon(poke))
+            for poke in cpuPokemon:
+                self.cpuPokemon.append(Pokemon(poke))
+
+    def printPokemon(self, isPlayer):
+        msg = ""
+        if isPlayer:
+            for i in range(0,len(self.userPokemon)):
+                if i != self.userActive and self.userPokemon[i].isAlive():
+                    msg += "\nSwitch to " + self.userPokemon[i].printHP()
+        else:
+            for i in range(0,len(self.cpuPokemon)):
+                if i != self.cpuActive and self.cpuPokemon[i].isAlive():
+                    msg += "\nSwitch to " + self.cpuPokemon[i].printHP()
+        return msg
+
+    def alive(self, isPlayer):
+        alive = False
+        if isPlayer:
+            for poke in self.userPokemon:
+                if poke.isAlive():
+                    alive = True 
+        else:
+            for poke in self.cpuPokemon:
+                if poke.isAlive():
+                    alive = True 
+        return alive
+
+    def switchPokemon(self, isPlayer, switch=True):
+        if isPlayer:
+            self.sprite1 = PhotoImage(file="Sprites/" + self.userPokemon[self.userActive].name + ".gif")
+            self.sprite1Label.configure(image=self.sprite1)
+            self.sprite1Label.image = self.sprite1
+
+            # Updating the info for both Pokemon after switching
+            self.moveText1.config(state=NORMAL)
+            self.moveText2.config(state=NORMAL)
+            self.moveText1.delete(0.0, END)
+            self.moveText2.delete(0.0, END)
+            self.moveText1.insert(0.0, self.userPokemon[self.userActive].printHP() + "\n" + self.userPokemon[self.userActive].printMoves() + self.printPokemon(True))
+            self.moveText2.insert(0.0, self.cpuPokemon[self.cpuActive].printHP() + "\n" + self.cpuPokemon[self.cpuActive].printMoves() + self.printPokemon(False))
+            self.moveText1.config(state=DISABLED)
+            self.moveText2.config(state=DISABLED)
+
+            if switch:
+                self.moveEnt1.delete(0, END)
+                self.moveEnt1.config(state=DISABLED)
+                self.moveBtn1.config(state=DISABLED)
+                self.moveEnt2.config(state=NORMAL)
+                self.moveBtn2.config(state=NORMAL)
             else:
-                self.txtStats.config(state=NORMAL)
-                self.txtStats.delete(0.0, END)
-                self.txtStats.insert(0.0, "Please make sure the Pokemon you've entered are in the Pokedex")
-                self.txtStats.config(state=DISABLED)
-                self.battleBtn.config(state=DISABLED)
+                self.moveEnt2.delete(0, END)
+                self.moveEnt2.config(state=DISABLED)
+                self.moveBtn2.config(state=DISABLED)
+                self.moveEnt1.config(state=NORMAL)
+                self.moveBtn1.config(state=NORMAL)
+        else:
+            self.sprite2 = PhotoImage(file="Sprites/" + self.cpuPokemon[self.cpuActive].name + ".gif")
+            self.sprite2Label.configure(image=self.sprite2)
+            self.sprite2Label.image = self.sprite2
+
+            # Updating the info for the other Pokemon
+            self.moveText1.config(state=NORMAL)
+            self.moveText2.config(state=NORMAL)
+            self.moveText1.delete(0.0, END)
+            self.moveText2.delete(0.0, END)
+            self.moveText1.insert(0.0, self.userPokemon[self.userActive].printHP() + "\n" + self.userPokemon[self.userActive].printMoves() + self.printPokemon(True))
+            self.moveText2.insert(0.0, self.cpuPokemon[self.cpuActive].printHP() + "\n" + self.cpuPokemon[self.cpuActive].printMoves() + self.printPokemon(False))
+            self.moveText1.config(state=DISABLED)
+            self.moveText2.config(state=DISABLED)
+
+            if switch:
+                self.moveEnt2.delete(0, END)
+                self.moveEnt2.config(state=DISABLED)
+                self.moveBtn2.config(state=DISABLED)
+                self.moveEnt1.config(state=NORMAL)
+                self.moveBtn1.config(state=NORMAL)
+            else:
+                self.moveEnt1.delete(0, END)
+                self.moveEnt1.config(state=DISABLED)
+                self.moveBtn1.config(state=DISABLED)
+                self.moveEnt2.config(state=NORMAL)
+                self.moveBtn2.config(state=NORMAL)
+
 
     # Creating a method to actually start the battle
     def beginBattle(self):
         # Replacing the blank image with the actual sprites of the appropriate Pokemon
         # Using the user-input string to determine which sprite image to use
-        self.sprite1 = PhotoImage(file="Sprites/" + self.userStrVar.get().lower() + ".gif")
+        self.sprite1 = PhotoImage(file="Sprites/" + self.userPokemon[self.userActive].name + ".gif")
         self.sprite1Label.configure(image=self.sprite1)
         self.sprite1Label.image = self.sprite1
 
-        self.sprite2 = PhotoImage(file="Sprites/" + self.cpuStrVar.get().lower() + ".gif")
+        self.sprite2 = PhotoImage(file="Sprites/" + self.cpuPokemon[self.cpuActive].name + ".gif")
         self.sprite2Label.configure(image=self.sprite2)
         self.sprite2Label.image = self.sprite2
 
@@ -138,17 +239,17 @@ class Application(Frame):
         self.moveText2.config(state=NORMAL)
         self.moveText1.delete(0.0, END)
         self.moveText2.delete(0.0, END)
-        self.moveText1.insert(0.0, self.userPokemon.printHP() + "\n" + self.userPokemon.printMoves())
-        self.moveText2.insert(0.0, self.cpuPokemon.printHP() + "\n" + self.cpuPokemon.printMoves())
+        self.moveText1.insert(0.0, self.userPokemon[self.userActive].printHP() + "\n" + self.userPokemon[self.userActive].printMoves() + self.printPokemon(True))
+        self.moveText2.insert(0.0, self.cpuPokemon[self.cpuActive].printHP() + "\n" + self.cpuPokemon[self.cpuActive].printMoves() + self.printPokemon(False))
         self.moveText1.config(state=DISABLED)
         self.moveText2.config(state=DISABLED)
 
         # Deciding which Pokemon to enable first based on the speed (battleSpeed) stat
-        if self.userPokemon.isAlive() and self.cpuPokemon.isAlive():
-            if self.userPokemon.battleSpeed >= self.cpuPokemon.battleSpeed:
+        if self.userPokemon[self.userActive].isAlive() and self.cpuPokemon[self.cpuActive].isAlive():
+            if self.userPokemon[self.userActive].battleSpeed >= self.cpuPokemon[self.cpuActive].battleSpeed:
                 self.moveEnt1.config(state=NORMAL)
                 self.moveBtn1.config(state=NORMAL)
-            elif self.cpuPokemon.battleSpeed > self.userPokemon.battleSpeed:
+            elif self.cpuPokemon[self.cpuActive].battleSpeed > self.userPokemon[self.userActive].battleSpeed:
                 self.moveEnt2.config(state=NORMAL)
                 self.moveBtn2.config(state=NORMAL)
         self.battleBtn.config(state=DISABLED)
@@ -156,27 +257,26 @@ class Application(Frame):
     # Method takes the user-inputted string and plugs it into the attack function
     # Prints the result of the attack function to the center text box
     def selectMove1(self):
-        if self.moveStrVar1.get().lower() in self.userPokemon.moveList:
+        if self.moveStrVar1.get().lower() in self.userPokemon[self.userActive].moveList:
             self.txtStats.config(state=NORMAL)
             self.txtStats.delete(0.0, END)
-            self.txtStats.insert(0.0, attack(self.moveStrVar1.get(), self.userPokemon, self.cpuPokemon))
+            self.txtStats.insert(0.0, attack(self.moveStrVar1.get(), self.userPokemon[self.userActive], self.cpuPokemon[self.cpuActive]))
             self.txtStats.config(state=DISABLED)
-
+            
             # Updating the info for both Pokemon after the move has been used
             self.moveText1.config(state=NORMAL)
             self.moveText2.config(state=NORMAL)
             self.moveText1.delete(0.0, END)
             self.moveText2.delete(0.0, END)
-            self.moveText1.insert(0.0, self.userPokemon.printHP() + "\n" + self.userPokemon.printMoves())
-            self.moveText2.insert(0.0, self.cpuPokemon.printHP() + "\n" + self.cpuPokemon.printMoves())
+            self.moveText1.insert(0.0, self.userPokemon[self.userActive].printHP() + "\n" + self.userPokemon[self.userActive].printMoves() + self.printPokemon(True))
+            self.moveText2.insert(0.0, self.cpuPokemon[self.cpuActive].printHP() + "\n" + self.cpuPokemon[self.cpuActive].printMoves() + self.printPokemon(False))
             self.moveText1.config(state=DISABLED)
             self.moveText2.config(state=DISABLED)
 
-            # If one of the Pokemon faints, this method will end the battle by disabling all other buttons/fields except
-            # for the Restart button
-            if not self.cpuPokemon.isAlive():
+            # if the other player loses, end the game
+            if not self.alive(False):
                 self.txtStats.config(state=NORMAL)
-                self.txtStats.insert(END, "\n" + self.cpuPokemon.faint())
+                self.txtStats.insert(END, "\n" + self.cpuPokemon[self.cpuActive].faint())
                 self.txtStats.insert(END, "\nPlay again?")
                 self.txtStats.config(state=DISABLED)
                 self.moveEnt1.delete(0, END)
@@ -184,22 +284,34 @@ class Application(Frame):
                 self.moveBtn1.config(state=DISABLED)
                 self.moveEnt1.config(state=DISABLED)
                 self.battleBtn.config(state=DISABLED)
-
-            # If both Pokemon are alive, this method will disable the current Pokemon and enable the opposing Pokemon so
-            # it can make its move as well
+            elif not self.cpuPokemon[self.cpuActive].isAlive():
+                # other player still has at least 1 more pokemon alive, switch to first alive pokemon
+                for i in range(0,len(self.cpuPokemon)):
+                    if self.cpuPokemon[i].isAlive():
+                        self.cpuActive = i
+                        break
+                self.switchPokemon(False,False)
             else:
+                # both pokemon are still alive
                 self.moveEnt1.delete(0, END)
                 self.moveEnt1.config(state=DISABLED)
                 self.moveBtn1.config(state=DISABLED)
                 self.moveEnt2.config(state=NORMAL)
                 self.moveBtn2.config(state=NORMAL)
+        else:
+            # handle switching
+            for i in range(0,len(self.userPokemon)):
+                if i != self.userActive and self.userPokemon[i].isAlive() and self.moveStrVar1.get().lower() == self.userPokemon[i].name.lower():
+                    self.userActive = i
+                    self.switchPokemon(True)
+                    break
 
     # Does the same thing as selectMove1() just with respect to the other Pokemon
     def selectMove2(self):
-        if self.moveStrVar2.get().lower() in self.cpuPokemon.moveList:
+        if self.moveStrVar2.get().lower() in self.cpuPokemon[self.cpuActive].moveList:
             self.txtStats.config(state=NORMAL)
             self.txtStats.delete(0.0, END)
-            self.txtStats.insert(0.0, attack(self.moveStrVar2.get(), self.cpuPokemon, self.userPokemon))
+            self.txtStats.insert(0.0, attack(self.moveStrVar2.get(), self.cpuPokemon[self.cpuActive], self.userPokemon[self.userActive]))
             self.txtStats.config(state=DISABLED)
 
             # Updating the info for the other Pokemon
@@ -207,14 +319,15 @@ class Application(Frame):
             self.moveText2.config(state=NORMAL)
             self.moveText1.delete(0.0, END)
             self.moveText2.delete(0.0, END)
-            self.moveText1.insert(0.0, self.userPokemon.printHP() + "\n" + self.userPokemon.printMoves())
-            self.moveText2.insert(0.0, self.cpuPokemon.printHP() + "\n" + self.cpuPokemon.printMoves())
+            self.moveText1.insert(0.0, self.userPokemon[self.userActive].printHP() + "\n" + self.userPokemon[self.userActive].printMoves() + self.printPokemon(True))
+            self.moveText2.insert(0.0, self.cpuPokemon[self.cpuActive].printHP() + "\n" + self.cpuPokemon[self.cpuActive].printMoves() + self.printPokemon(False))
             self.moveText1.config(state=DISABLED)
             self.moveText2.config(state=DISABLED)
 
-            if not self.userPokemon.isAlive():
+            # if the other player loses, end the game
+            if not self.alive(True):
                 self.txtStats.config(state=NORMAL)
-                self.txtStats.insert(END, "\n" + self.userPokemon.faint())
+                self.txtStats.insert(END, "\n" + self.userPokemon[self.userActive].faint())
                 self.txtStats.insert(END, "\nPlay again?")
                 self.txtStats.config(state=DISABLED)
                 self.moveEnt2.delete(0, END)
@@ -224,19 +337,35 @@ class Application(Frame):
                 self.moveEnt2.config(state=DISABLED)
                 self.moveBtn2.config(state=DISABLED)
                 self.battleBtn.config(state=DISABLED)
-
+            elif not self.userPokemon[self.userActive].isAlive():
+                # other player still has at least 1 more pokemon alive, switch to first alive pokemon
+                for i in range(0,len(self.userPokemon)):
+                    if self.userPokemon[i].isAlive():
+                        self.userActive = i
+                        break
+                self.switchPokemon(True,False)
             else:
                 self.moveEnt2.delete(0, END)
                 self.moveEnt2.config(state=DISABLED)
                 self.moveBtn2.config(state=DISABLED)
                 self.moveEnt1.config(state=NORMAL)
                 self.moveBtn1.config(state=NORMAL)
+        else:
+            # handle switching
+            for i in range(0,len(self.cpuPokemon)):
+                if i != self.cpuActive and self.cpuPokemon[i].isAlive() and self.moveStrVar2.get().lower() == self.cpuPokemon[i].name.lower():
+                    self.cpuActive = i
+                    validPokemon = True
+                    self.switchPokemon(False)
+                    break
 
     # Completely clears and resets all text fields, buttons, and images to their original state
     def restart(self):
         # Resetting the Pokemon objects
-        self.userPokemon = None
-        self.cpuPokemon = None
+        self.userPokemon = []
+        self.cpuPokemon = []
+        self.userActive = 0;
+        self.cpuActive = 0;
 
         # Resetting all the widgets
         self.txtStats.config(state=NORMAL)
